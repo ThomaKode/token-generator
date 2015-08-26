0b4c6f43d1dcdaccbf72ca0195aa00c731ace36b
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-08-26 19:08:15 +0000

    Added the first snippet bit of a file that can merge relevant PDF files within a a specific repository,

---
+++ pdfmerge.py
@@ -0,0 +1,8 @@
+# Python Source Code
+        ######## Section A #########
+        # specify App SID
+AsposeApp.app_sid = '5a******-***-***-8a***'
+        # Specify App Key
+AsposeApp.app_key = '***********3g'
+        # bbuild URI  to merge PDFs
+str_uri = 'http://api.aspose.com/v1.1/words/MainDocument.docx/appendDocument'
