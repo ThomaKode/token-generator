860f37085883f0848d9f6dc0277cd153842d2488
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-08-27 22:31:26 +0000

    Updated pdfmerge.py

--- pdfmerge.py
+++ pdfmerge.py
@@ -4,5 +4,25 @@
 AsposeApp.app_sid = '5a******-***-***-8a***'
         # Specify App Key
 AsposeApp.app_key = '***********3g'
-        # bbuild URI  to merge PDFs
+        # build URI  to merge PDFs
 str_uri = 'http://api.aspose.com/v1.1/words/MainDocument.docx/appendDocument'
+        # sign URI
+signed_uri = Utils.sign(Utils(), str_uri)
+
+        ######## End Section A #########
+
+
+        ######## Section B ##########
+        #Build JSON to post
+        json_document1 = json.dumps({'Href' : 'AppendDocument1.docx',
+'ImportFormatMode' : 'KeepSourceFormatting'})
+        json_document2 = json.dumps({'Href' : 'AppendDocument2.docx',
+'ImportFormatMode' : 'UseDestinationStyles'})
+json_arr = '{\'DocumentEntries\':[' + json_document1 + ',' + json_document2 +
+']}'
+
+        ######### End Section B ########
+
+            ##### Section C ######
+Utils.process_command(Utils(), signed_uri, 'POST', 'JSON', json_arr)
+            ##### End Section C ######                
