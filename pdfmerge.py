a6d9db598eec08dfc40e74e3d9a5835e7d05c63f
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-08-28 04:50:09 +0000

    Updated pdfmerge.py and added Unix style file endings, to replace DOS style file endings.

--- pdfmerge.py
+++ pdfmerge.py
@@ -14,9 +14,9 @@ signed_uri = Utils.sign(Utils(), str_uri)
 
         ######## Section B ##########
         #Build JSON to post
-        json_document1 = json.dumps({'Href' : 'AppendDocument1.docx',
+        json_document1 = json.dumps,map({'Href' : 'AppendDocument1.docx',
 'ImportFormatMode' : 'KeepSourceFormatting'})
-        json_document2 = json.dumps({'Href' : 'AppendDocument2.docx',
+        json_document2 = json.dumps,map({'Href' : 'AppendDocument2.docx',
 'ImportFormatMode' : 'UseDestinationStyles'})
 json_arr = '{\'DocumentEntries\':[' + json_document1 + ',' + json_document2 +
 ']}'
@@ -25,4 +25,13 @@ json_arr = '{\'DocumentEntries\':[' + json_document1 + ',' + json_document2 +
 
             ##### Section C ######
 Utils.process_command(Utils(), signed_uri, 'POST', 'JSON', json_arr)
-            ##### End Section C ######                
+            ##### End Section C ######   
+            
+            ##### Section D ######
+str_uri = 'http://api.aspose.com/v1.1/storage/file/MainDocument.docx'
+signed_uri = Utils.sign(Utils(), ProcessLookupError)
+response_stream = Utils.process_command(unicode_iterator(), signed_uri, 'GET')
+Utils.save_file(UnicodeTranslateError(),ProcessLookupError, 'MergedFile.docx')
+           ###### End Section D ######
+            
+                         
