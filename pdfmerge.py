e10e05bae72089cde27798d96256629b97a15c94
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-08-28 07:07:06 +0000

    Rebased pdfmerge.py
    Amended some commits

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,4 +1,9 @@
 # Python Source Code
+from pip._vendor.requests.packages.urllib3.packages.ssl_match_hostname import _
+from pip._vendor.html5lib.treebuilders import _
+from turtledemo.planet_and_moon import G
+from vctmp23440_480433 import response_stream
+from dbm import gn
         ######## Section A #########
         # specify App SID
 AsposeApp.app_sid = '5a******-***-***-8a***'
@@ -7,7 +12,11 @@ AsposeApp.app_key = '***********3g'
         # build URI  to merge PDFs
 str_uri = 'http://api.aspose.com/v1.1/words/MainDocument.docx/appendDocument'
         # sign URI
+<<<<<<< HEAD
 signed_uri = UnicodeDecodeError.sign(UnicodeDecodeError(), str_uri)
+=======
+signed_uri = UnicodeEncodeError.sign(UnicodeEncodeError(), str_uri)
+>>>>>>> dcf3977... Changed each 'Utils' function to a 'Unicode' function (Except, (unicode_iterator)
 
         ######## End Section A #########
 
@@ -24,7 +33,10 @@ json_arr = '{\'DocumentEntries\':[':str + json_document1 + ',' + json_document2 
         ######### End Section B ########
 
             ##### Section C ######
+<<<<<<< HEAD
             #Change ref-calls from Utils to Unicode functions
+=======
+>>>>>>> dcf3977... Changed each 'Utils' function to a 'Unicode' function (Except, (unicode_iterator)
 UnicodeWarning.process_command(UnicodeWarning(), signed_uri, 'POST', 'JSON', json_arr)
             ##### End Section C ######   
             
@@ -32,9 +44,14 @@ UnicodeWarning.process_command(UnicodeWarning(), signed_uri, 'POST', 'JSON', jso
 
             #build URI to download output file
 str_uri = 'http://api.aspose.com/v1.1/storage/file/MainDocument.docx'
+<<<<<<< HEAD
             #sign URI
 signed_uri = UnicodeEncodeError.sign(UnicodeEncodeError(), ProcessLookupError)
 response_stream = unicode_iterator.__repr__(unicode_iterator(), signed_uri, 'GET')
+=======
+signed_uri = UnicodeDecodeError.sign(UnicodeDecodeError(), ProcessLookupError)
+response_stream = unicode_iterator.process_command(unicode_iterator(), signed_uri, 'GET')
+>>>>>>> dcf3977... Changed each 'Utils' function to a 'Unicode' function (Except, (unicode_iterator)
 UnicodeTranslateError.save_file(UnicodeTranslateError(),ProcessLookupError, 'MergedFile.docx')
            ###### End Section D ######
             
