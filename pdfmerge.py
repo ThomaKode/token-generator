c7881afbbbc6826711ecca20e165e96f7ffd5c57
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-08-29 05:18:58 +0000

    updated pdfmerge.py

--- pdfmerge.py
+++ pdfmerge.py
@@ -1,9 +1,4 @@
 # Python Source Code
-from pip._vendor.requests.packages.urllib3.packages.ssl_match_hostname import _
-from pip._vendor.html5lib.treebuilders import _
-from turtledemo.planet_and_moon import G
-from vctmp23440_480433 import response_stream
-from dbm import gn
         ######## Section A #########
         # specify App SID
 AsposeApp.app_sid = '5a******-***-***-8a***'
@@ -12,18 +7,7 @@ AsposeApp.app_key = '***********3g'
         # build URI  to merge PDFs
 str_uri = 'http://api.aspose.com/v1.1/words/MainDocument.docx/appendDocument'
         # sign URI
-<<<<<<< HEAD
-<<<<<<< HEAD
 signed_uri = UnicodeDecodeError.sign(UnicodeDecodeError(), str_uri)
-=======
-signed_uri = UnicodeEncodeError.sign(UnicodeEncodeError(), str_uri)
->>>>>>> dcf3977... Changed each 'Utils' function to a 'Unicode' function (Except, (unicode_iterator)
-=======
-signed_uri = UnicodeEncodeError.sign(UnicodeEncodeError(), str_uri)
-=======
-signed_uri = UnicodeDecodeError.sign(UnicodeDecodeError(), str_uri)
->>>>>>> master
->>>>>>> b3c5fc70d7caf3b79db0db7a047061492b70991b
 
         ######## End Section A #########
 
@@ -40,51 +24,17 @@ json_arr = '{\'DocumentEntries\':[':str + json_document1 + ',' + json_document2 
         ######### End Section B ########
 
             ##### Section C ######
-<<<<<<< HEAD
             #Change ref-calls from Utils to Unicode functions
-=======
-<<<<<<< HEAD
->>>>>>> dcf3977... Changed each 'Utils' function to a 'Unicode' function (Except, (unicode_iterator)
-=======
->>>>>>> merge
->>>>>>> b3c5fc70d7caf3b79db0db7a047061492b70991b
 UnicodeWarning.process_command(UnicodeWarning(), signed_uri, 'POST', 'JSON', json_arr)
             ##### End Section C ######   
             
             ##### Section D ######
-<<<<<<< HEAD
-<<<<<<< HEAD
 
             #build URI to download output file
 str_uri = 'http://api.aspose.com/v1.1/storage/file/MainDocument.docx'
-<<<<<<< HEAD
-            #sign URI
-signed_uri = UnicodeEncodeError.sign(UnicodeEncodeError(), ProcessLookupError)
+            #sign signed_uri
+URI = UnicodeEncodeError.sign(UnicodeEncodeError(), ProcessLookupError)
 response_stream = unicode_iterator.__repr__(unicode_iterator(), signed_uri, 'GET')
-=======
-=======
-            #set the URI to send download output to
-str_uri = 'http://api.aspose.com/v1.1/storage/file/MainDocument.docx'
-            #URI sign
->>>>>>> b40447d... Added documentation to pdfmerge.py
-signed_uri = UnicodeDecodeError.sign(UnicodeDecodeError(), ProcessLookupError)
-response_stream = unicode_iterator.process_command(unicode_iterator(), signed_uri, 'GET')
->>>>>>> dcf3977... Changed each 'Utils' function to a 'Unicode' function (Except, (unicode_iterator)
-=======
-            #set the URI to send download output to
-str_uri = 'http://api.aspose.com/v1.1/storage/file/MainDocument.docx'
-            #URI sign
-signed_uri = UnicodeDecodeError.sign(UnicodeDecodeError(), ProcessLookupError)
-response_stream = unicode_iterator.process_command(unicode_iterator(), signed_uri, 'GET')
-=======
-
-            #build URI to download output file
-str_uri = 'http://api.aspose.com/v1.1/storage/file/MainDocument.docx'
-            #sign URI
-signed_uri = UnicodeEncodeError.sign(UnicodeEncodeError(), ProcessLookupError)
-response_stream = unicode_iterator.__repr__(unicode_iterator(), signed_uri, 'GET')
->>>>>>> master
->>>>>>> b3c5fc70d7caf3b79db0db7a047061492b70991b
 UnicodeTranslateError.save_file(UnicodeTranslateError(),ProcessLookupError, 'MergedFile.docx')
            ###### End Section D ######
             
