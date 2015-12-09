0f266af02cdb3cfb020947f2ca7457f9fea54122
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-12-09 01:25:22 +0000

    Update file roken-generator.py

--- token-generate.py
+++ token-generate.py
@@ -1,18 +1,35 @@
 import hmac
 import math
+import hashlib
 
 from hashlib import md5
+
+class MessageBox():    
 #---pre: encode values
-
-a = hmac.new("evilbillionaire:006da874-4268-4a07-940f-58e06ee36aa2",
+    def name(a): 
+       a = hmac.new("evilbillionaire:006da874-4268-4a07-940f-58e06ee36aa2",
          "ad5540553b02489586ae7ebbbd2562a8", md5).hexdigest()
+         
+    print ('The value of x*a {}.'.format(math.fsum))
 
-b = hashlib.md5()
-b.update(b"This is the string to be encrypted")
-b.update(b"concatenated to this string")
-b.digest()
+    def name(b):
+        b = hashlib.md5() 
+        b.update(b"This is the string to be encrypted")
+        b.update(b"concatenated to this string")
+        b.digest()
 
+class Display():
+ 
+
+   
 #--post: Print result to file
-f = open('workspace', 'r+b') #Create a file object 'f' for reading and writing
+    def name(f):
+        f = open('token.key', 'w') #Create a file object 'f' for reading and writing
+                                #in binary mode
+    f.write('a')
 #in binary mode
-print f(a)
+f.read(type)
+print f(a(b))
+list(f).read
+for line in f:
+    print line or list
