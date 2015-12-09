8862ef29bb7586d8ab3708ebca1506984a1c3c5a
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-12-09 00:02:36 +0000

    Update file roken-generate.py

--- token-generate.py
+++ token-generate.py
@@ -1,4 +1,18 @@
 import hmac
+import math
+
 from hashlib import md5
-hmac.new("evilbillionaire:006da874-4268-4a07-940f-58e06ee36aa2",
+#---pre: encode values
+
+a = hmac.new("evilbillionaire:006da874-4268-4a07-940f-58e06ee36aa2",
          "ad5540553b02489586ae7ebbbd2562a8", md5).hexdigest()
+
+b = hashlib.md5()
+b.update(b"This is the string to be encrypted")
+b.update(b"concatenated to this string")
+b.digest()
+
+#--post: Print result to file
+f = open('workspace', 'r+b') #Create a file object 'f' for reading and writing
+#in binary mode
+print f(a)
